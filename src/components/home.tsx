ca6b010d3298315d98adb52b92c5d84476abbdb9
Quentin1402 <113422793+Quentin1402@users.noreply.github.com>
2023-01-10 22:25:27 +0100

    Delete src directory

--- src/components/home.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-    return(
-        <div id="home">
-            <h1>Bienvenue sur le meilleur site de réservation de vols au monde</h1>
-        </div>
-    )
-}
